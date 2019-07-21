850d107fed7e1d1946b245dcd4a4fb059738cf62
Kuba Wojciechowski <catchthemlive@gmail.com>
2019-07-21 19:28:08 +0200

    refactor Artist Model and tests

--- core/main/models.py
+++ core/main/models.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 import os
 
 
-def content_file_name_artist(instance, filename):
+def content_file_name_artist_thunbnail(instance, filename):
     ext = filename.split('.')[-1]
     filename = f'{instance.name}_thumbnail.{ext}'
     return os.path.join('artist_thumbnail/', filename)
@@ -19,7 +19,7 @@ class Artist(models.Model):
     name = models.CharField(max_length=128)
     desription = models.TextField(null=True, blank=True)
     year = models.IntegerField(null=True, blank=True)
-    thumbnail = models.ImageField(upload_to=content_file_name_artist, null=True, blank=True)
+    thumbnail = models.ImageField(upload_to=content_file_name_artist_thunbnail, null=True, blank=True)
     logo = models.ImageField(upload_to=content_file_name_artist_logo, null=True, blank=True)
     facebook = models.URLField(null=True, blank=True)
     spotify = models.URLField(null=True, blank=True)
@@ -33,5 +33,6 @@ class Artist(models.Model):
 
 @receiver(post_delete, sender=Artist)
 def submission_delete(sender, instance, **kwargs):
+    """Deleting Artist images when instance will deleting"""
     instance.thumbnail.delete(False)
     instance.logo.delete(False)
--- core/main/tests.py
+++ core/main/tests.py
@@ -4,33 +4,6 @@ from .models import Artist
 import os
 
 
-TEXT_SAMPLE_LONG_TEXT = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare malesuada
-eros. Mauris nec lacus vulputate, maximus sapien et, lacinia turpis.
-In hendrerit nibh ac eros consequat, at cursus ipsum tincidunt.
-Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-Pellentesque porta faucibus venenatis. Fusce urna massa, lacinia nec ex id, placerat condimentum lacus.
-Nullam hendrerit ipsum ac nisi volutpat, nec tincidunt turpis scelerisque.
-Nulla sit amet volutpat eros.
-Integer at est sagittis, iaculis nulla eget, convallis tellus.
-Phasellus facilisis, nulla et tempus interdum, lacus quam lobortis purus, eu congue leo mauris vitae urna.
-Duis porttitor efficitur metus et suscipit. Suspendisse mattis elit id hendrerit imperdiet.
-
-Integer vehicula porttitor quam. Vestibulum venenatis sem sit amet odio consectetur lacinia.
-Vestibulum et scelerisque mauris. Nam ultrices sagittis eros, sed suscipit massa tristique eget.
-Phasellus non commodo dui. Nam enim libero, placerat euismod metus ut, scelerisque vehicula sem.
-Donec consectetur erat id metus rhoncus blandit. Maecenas ultrices libero a pharetra ullamcorper.
-Pellentesque blandit libero luctus, faucibus turpis non, tempor enim. Aenean mauris magna, efficitur vitae egestas eget, auctor congue lacus.
-Nulla nec justo eu lacus faucibus semper. Suspendisse sollicitudin, lectus eget faucibus rhoncus, nulla libero mollis tellus, in finibus mauris ipsum consequat nibh.
-Quisque fermentum velit sapien, ut congue metus laoreet facilisis. Aliquam mattis diam quis eros porta, a rutrum quam imperdiet. Etiam ornare ipsum eu erat semper mollis.
-
-Sed at nibh fringilla, porttitor tortor nec, mattis urna.
-Vestibulum enim nibh, tempor ut tortor vel, lacinia ultricies leo.
-In at lectus sollicitudin, eleifend massa id, aliquet augue.
-Curabitur ipsum nunc, bibendum ut hendrerit quis, vehicula dignissim nibh.
-Suspendisse hendrerit sed purus a imperdiet.
-Donec vel convallis nisl. Morbi magna augue, volutpat nec vehicula vel, auctor a risus.
-"""
-
 class TestArtistModel(TestCase):
     """Test Artist Model"""
 
@@ -40,7 +13,8 @@ class TestArtistModel(TestCase):
         artist1 = Artist.objects.create(
             id=1,
             name = 'Chilli Crew',
-            desription = TEXT_SAMPLE_LONG_TEXT,
+            desription = """Integer vehicula porttitor quam. Vestibulum venenatis sem sit amet odio consectetur lacinia.
+            Vestibulum et scelerisque mauris. Nam ultrices sagittis eros, sed suscipit massa tristique eget.""",
             year = 2018,
             thumbnail = SimpleUploadedFile(name='test_image.jpg', content=open('/app/core/main/test.jpg', 'rb').read(), content_type='image/jpeg'),
             logo = SimpleUploadedFile(name='test_image2.jpg', content=open('/app/core/main/test.jpg', 'rb').read(), content_type='image/jpeg'),
@@ -55,7 +29,8 @@ class TestArtistModel(TestCase):
         artist2 = Artist.objects.create(
             id=2,
             name = 'Pikes',
-            desription = TEXT_SAMPLE_LONG_TEXT,
+            desription = """Integer vehicula porttitor quam. Vestibulum venenatis sem sit amet odio consectetur lacinia.
+            Vestibulum et scelerisque mauris. Nam ultrices sagittis eros, sed suscipit massa tristique eget.""",
             year = 2017,
             thumbnail = SimpleUploadedFile(name='test_image.jpg', content=open('/app/core/main/test.jpg', 'rb').read(), content_type='image/jpeg'),
             logo = SimpleUploadedFile(name='test_image2.jpg', content=open('/app/core/main/test.jpg', 'rb').read(), content_type='image/jpeg'),
@@ -74,17 +49,13 @@ class TestArtistModel(TestCase):
         self.assertNotIn(artist3, artists) # test that artist3 is not Artist instance
         self.assertEqual(len(artists), 2) # test created 2 instance
         self.assertEqual(artist1.name, 'Chilli Crew') # test valid data
-        self.assertEqual(artist2.name, 'Pikes') # test valid data
         self.assertEqual(artist1.year, 2018) # test valid type
-        self.assertEqual(artist2.year, 2017) # test valid type
         self.assertEqual(str(artist1), 'Chilli Crew') #string representacion
-        self.assertEqual(str(artist2), 'Pikes') #string representacion
         self.assertEqual(str(type(artist1.thumbnail)), "<class 'django.db.models.fields.files.ImageFieldFile'>") # test image type
-        self.assertEqual(str(type(artist2.thumbnail)), "<class 'django.db.models.fields.files.ImageFieldFile'>") # test image type
         self.assertEqual(artist1.thumbnail.path, f'/app/core/artist_thumbnail/Chilli_Crew_thumbnail.jpg') # test valid new file name
 
         artist1.delete()
         artist2.delete()
         artists = Artist.objects.all()
         self.assertEqual(len(artists), 0) # test delete instance
-        self.assertFalse(os.path.isfile('/app/core/artist_thumbnail/Chilli_Crew_thumbnail.jpg'))
+        self.assertFalse(os.path.isfile('/app/core/artist_thumbnail/Chilli_Crew_thumbnail.jpg')) # test deleting instance images when will deleting instance
